f13e26ccd1d6a855d8b5de0af9f9885a40810e76
Lucas Soares <142176643+Lucc337@users.noreply.github.com>
2025-06-12 13:27:51 -0300

    Update books.js

--- BibliotecaTrabalho/routes/books.js
+++ BibliotecaTrabalho/routes/books.js
@@ -4,10 +4,10 @@ const bookController = require('../controllers/bookController');
 const verifyToken = require('../middleware/authMiddleware');
 
 router.get('/', verifyToken, bookController.listBooks);
-router.get('/create', verifyToken, bookController.showCreateForm);
-router.post('/create', verifyToken, bookController.createBook);
-router.get('/edit/:id', verifyToken, bookController.showEditForm);
-router.post('/edit/:id', verifyToken, bookController.updateBook);
+router.get('/create', verifyToken, bookController.getCreateBook);
+router.post('/create', verifyToken, bookController.postCreateBook);
+router.get('/edit/:id', verifyToken, bookController.getEditBook);
+router.post('/edit/:id', verifyToken, bookController.postEditBook);
 router.get('/delete/:id', verifyToken, bookController.deleteBook);
 
 module.exports = router;
