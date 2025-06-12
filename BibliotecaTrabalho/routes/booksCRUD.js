1643b6942b6fce469c0c3b8f342e61992ca9545b
Lucas Soares <142176643+Lucc337@users.noreply.github.com>
2025-06-12 12:33:41 -0300

    Add files via upload

---
+++ BibliotecaTrabalho/routes/booksCRUD.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('../middleware/authMiddleware');
+const bookController = require('../controllers/bookController');
+
+
+router.get('/', verifyToken, bookController.listBooks);
+router.get('/create', verifyToken, bookController.getCreateBook);
+router.post('/create', verifyToken, bookController.postCreateBook);
+router.get('/edit/:id', verifyToken, bookController.getEditBook);
+router.post('/edit/:id', verifyToken, bookController.postEditBook);
+router.get('/delete/:id', verifyToken, bookController.deleteBook);
+
+module.exports = router;
